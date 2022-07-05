575cff2a55b3cc73a75976faabfb90ded56a7486
cnotv <giuseppe.leo@suse.com>
2022-07-05 16:38:33 +0200

    Add remaining E2E tests for the cluster

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -8,7 +8,27 @@ declare global {
     interface Chainable {
       login(username?: string, password?: string, cacheSession?: boolean): Chainable<Element>;
       byLabel(label: string,): Chainable<Element>;
+
+      /**
+       *  Wrapper for cy.get() to simply define the data-testid value that allows you to pass a matcher to find the element.
+       * @param id Value used for the data-testid attribute of the element.
+       * @param matcher Matching character used for attribute value:
+       * - `$`: Suffixed with this value
+       * - `^`: Prefixed with this value
+       * - `~`: Contains this value as whitespace separated words
+       * - `*`: Contains this value
+       */
       getId(id: string, matcher?: Matcher): Chainable<Element>;
+
+      /**
+       *  Wrapper for cy.find() to simply define the data-testid value that allows you to pass a matcher to find the element.
+       * @param id Value used for the data-testid attribute of the element.
+       * @param matcher Matching character used for attribute value:
+       * - `$`: Suffixed with this value
+       * - `^`: Prefixed with this value
+       * - `~`: Contains this value as whitespace separated words
+       * - `*`: Contains this value
+       */
       findId(id: string, matcher?: Matcher): Chainable<Element>;
     }
   }
